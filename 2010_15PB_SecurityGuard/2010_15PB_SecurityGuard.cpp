7f9831f2d8cedcb4939ec62fcde8c70942d6ab8a
mhws2 <mhws2@MHWS2>
2020-10-30 20:06:46 +0800

    添加项目

--- 2010_15PB_SecurityGuard/2010_15PB_SecurityGuard.cpp
+++ 2010_15PB_SecurityGuard/2010_15PB_SecurityGuard.cpp
@@ -6,6 +6,7 @@
 
 #include "..\Dll_API\CApi.h"
 #include "..\Dll_API\CPE.h"
+#include "..\Dll_API\MYINFO.h"
 #pragma comment(lib,"..\\x64\\debug\\Dll_API.lib")
 
 
@@ -14,22 +15,15 @@ int main()
 {
 	std::cout << "Hello World!\n";
 	char md5ch[33];
-	//char* filepath = (char*)"D:\\Videos\\20200923_脱口秀大会第三季.mp4";
-	//char* filepath = (char*)
-	//	"D:\\Videos\\七宗罪.Se7en.1995.BD-1080p.1920X1080.中英双语-电波字幕组.mkv";
-	//DWORD time = GetMd5_ByCertutil((char*)
-	//	"D:\\Videos\\20200923_脱口秀大会第三季.mp4", md5ch);
-	char* filepath = (char*)"D:\\15pb\\课堂\\20Q3\\0930_DLL\\Debug\\0930_DLL.dll";
+	char* filepath = DLLP1;
 	__int64 fSize = 0;
 	char* pFile = (char*)GetFilePtr(filepath, fSize, md5ch);
-	printf_s("文件名：%s\n", filepath);
 	CPE cPE = CPE(pFile, fSize);
 	//cPE.GetNTHeadInfo(true);
 	cPE.RvaToFoa(0);
 	cPE.~CPE();
-	filepath = (char*)"D:\\15pb\\课堂\\20Q3\\0930_DLL\\x64\\Debug\\0930_DLL.dll";
+	filepath = DLLP2;
 	pFile = (char*)GetFilePtr(filepath, fSize, md5ch);
-	printf_s("文件名：%s\n", filepath);
 	cPE = CPE(pFile, fSize);
 	//cPE.GetNTHeadInfo(true);
 	cPE.RvaToFoa(0);
